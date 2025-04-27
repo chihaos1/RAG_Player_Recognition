438ccefd784a061590f84cc9ead0e7c9050d8369
unknown <chihaos0629@gmail.com>
2025-04-27 13:41:50 -0400

    Adding scrape image API call

--- frontend/src/components/utils/api/scrape.tsx
+++ frontend/src/components/utils/api/scrape.tsx
@@ -3,5 +3,7 @@ import { get } from '../http.ts'
 
 
 export default function scrape_images() {
-    const data = (fetch('http://127.0.0.1:8082/scrape/images')) as unknown;
+    const data = (fetch('http://127.0.0.1:8082/scrape/images', {
+        
+    })) as unknown;
 }
